fd3b6f92ba026af33c5830144a559c214123c974
李晨 <lichen@fissoft.com>
2015-10-09 18:48:36 +0800

    3123

--- WebCode/WebCode/Scripts/Main/Main.js
+++ WebCode/WebCode/Scripts/Main/Main.js
@@ -1,7 +1,5 @@
 ﻿
-require(["jquery", "bootstrap", "//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js"], function($, bootstrap, highlight) {
-    
-
-
-
+require(["jquery", "bootstrap", "//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js"], function ($, bootstrap, highlight) {
+    //TODO 登录注册 按钮工具悬浮提示
+    $("[data-toggle=\"tooltip\"]").tooltip();
 });
